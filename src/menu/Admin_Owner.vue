34d1dc71090bda6070e4074908740999462c93fe
Bigbabol1028 <2281425627@qq.com>
2023-07-05 22:06:08 +0800

    table

--- src/menu/Admin_Owner.vue
+++ src/menu/Admin_Owner.vue
@@ -7,36 +7,39 @@
         </div>
         <div class="tabback">
             <el-table :data="tableData1" border>
-                <el-table-column prop="number" label="序号" width="130px"/>
-                <el-table-column prop="name" label="名称" width="200px"/>
-                <el-table-column prop="address" label="地址" width="450px"/>
-                <el-table-column prop="state" label="验证状态" width="200px"/>
-                <el-table-column prop="account" label="云钱包账号" width="300px"/>
-                <el-table-column prop="quantity" label="已应用的权限数量" width="200px"/>
+                <el-table-column prop="number" label="序号" width="130px" />
+                <el-table-column prop="name" label="名称" width="200px" />
+                <el-table-column prop="address" label="地址" width="450px" />
+                <el-table-column prop="state_text" label="验证状态" width="200px" />
+                <el-table-column prop="account" label="云钱包账号" width="300px" />
+                <el-table-column prop="quantity" label="已应用的权限数量" width="200px" />
             </el-table>
         </div>
         <br>
         <h1>合约权限</h1>
         <div class="tabback">
             <el-table :data="tableData2" border style="width: 100%">
-                <el-table-column prop="contract" label="合约" width="574px"/>
-                <el-table-column prop="permissions" label="权限" width="162px"/>
-                <el-table-column prop="address" label="权限地址" width="296px"/>
-                <el-table-column prop="settime" label="设置时间" width="262px"/>
-                <el-table-column prop="operate" label="操作" width="154px"/>
+                <el-table-column prop="contract" label="合约" width="574px" />
+                <el-table-column prop="permissions" label="权限" width="162px" />
+                <el-table-column prop="address" label="权限地址" width="296px" />
+                <el-table-column prop="settime" label="设置时间" width="262px" />
+                <el-table-column prop="operate" label="操作" width="154px" />
             </el-table>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { tableV2GridProps } from 'element-plus/es/components/table-v2/src/grid';
+
 
 const tableData1 = [
     {
         number: '1',
         name: '[项目管理员账户]',
         address: '0x22704345C8649CA4b9f970c616cE7fb5fB1Fdab9',
-        state: '已验证',
+        state: true,
+        state_text: '',
         account: '234567@abc.com',
         quantity: '12'
     },
@@ -44,7 +47,8 @@ const tableData1 = [
         number: '2',
         name: 'abc',
         address: '0xdFb630379ccB11114c31D9e618eB6D1275eDDBCf',
-        state: '已验证',
+        state: true,
+        state_text: '',
         account: '234567@abc.com',
         quantity: '0'
     },
@@ -52,11 +56,21 @@ const tableData1 = [
         number: '3',
         name: '-',
         address: '0x40C7214f2d1312A492DBBc77Bb1D851431ad1678',
-        state: '未验证',
+        state: false,
+        state_text: '',
         account: '-',
         quantity: '2'
     }
 ]
+let i = 0
+
+for (i = 0; i < tableData1.length; i++) {
+    if (tableData1[i].state == true)
+        tableData1[i].state_text = "已验证";
+    else
+        tableData1[i].state_text = "未验证";
+
+}
 
 const tableData2 = [
     {
